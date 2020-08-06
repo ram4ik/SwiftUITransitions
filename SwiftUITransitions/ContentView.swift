76aaded55277b5773ef9ef9ea1891e7c0d98ebc9
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-06 11:49:33 +0300

    init project

--- SwiftUITransitions/ContentView.swift
+++ SwiftUITransitions/ContentView.swift
@@ -7,10 +7,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MyText: View {
+    let text: String
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(text)
+            .font(.largeTitle)
             .padding()
+    }
+    
+    init(_ text: String) { self.text = text }
+}
+
+struct ContentView: View {
+    @State var isOn = true
+    
+    var body: some View {
+        VStack {
+            if isOn {
+                Spacer()
+                
+                MyText("Slide").transition(.slide)
+                
+                MyText("Opacity").transition(.opacity)
+                
+                MyText("Scale").transition(.scale)
+                
+                MyText("Move bottom").transition(.move(edge: .bottom))
+                
+                MyText("Offset (50, 50)").transition(.offset(x: 50, y: 50))
+                
+                MyText("Slide in scale out").transition(.asymmetric(insertion: .slide, removal: .scale))
+                
+                MyText("Slide and opacity").transition(AnyTransition.slide.combined(with: .opacity))
+            }
+            
+            Spacer()
+            
+            Button(action: {
+                withAnimation(.easeInOut(duration: 2)) {
+                    self.isOn.toggle()
+                }
+            }, label: {
+                Text("Toggle").font(.largeTitle)
+            })
+        }
     }
 }
 
