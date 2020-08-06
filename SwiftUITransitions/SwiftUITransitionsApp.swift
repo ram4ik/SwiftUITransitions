178e7a5228d19ba90bd910fe4d07565a5d75ca24
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-06 11:35:35 +0300

    Initial Commit

---
+++ SwiftUITransitions/SwiftUITransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITransitionsApp.swift
+//  SwiftUITransitions
+//
+//  Created by ramil on 06.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
